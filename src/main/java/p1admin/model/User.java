07ebe6cc347dae68e0807ca7a7437885b34a21ea
Caparrini <Caparrini@users.noreply.github.com>
2016-03-08 11:50:22 +0100

    Add User and Coordinates class

---
+++ src/main/java/p1admin/model/User.java
@@ -0,0 +1,51 @@
+package p1admin.model;
+import java.io.File;
+import java.util.Date;
+import java.util.ArrayList;
+
+public class User {
+	//Clase usuario de la aplicación
+	
+	private enum Gender{
+		MALE,
+		FEMALE
+	}
+	
+	private enum OtherGender{
+
+		MALE,
+		FEMALE,
+		BOTH
+	}
+	
+	
+	private String email;
+	private String password;
+	private String name;
+	
+	private Gender gender;
+	private OtherGender otherGender;
+	
+	private Date birthDate;
+	private String description;
+	private Coordinates coordinates;
+	
+	private ArrayList<String> interests;
+	private ArrayList<User> friends;
+	
+	private File profileImage;
+
+	
+	//TODO Array de mensajes
+	
+	
+	public User(String email,String password){
+		this.email = email;
+		this.password = password;
+	}
+	
+	
+	
+	
+
+}
