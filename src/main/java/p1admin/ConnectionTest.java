abd91099f7b3006c3cc079fd1a48d3d6e9c0730b
ergl <ergl@users.noreply.github.com>
2016-03-28 22:34:55 +0200

    Add DBFacade.{deleteAnswer, deleteQuestion, insertAnswer, updateQuestion}

--- src/main/java/p1admin/ConnectionTest.java
+++ src/main/java/p1admin/ConnectionTest.java
@@ -1,9 +1,12 @@
 package p1admin;
 
-import p1admin.adminDB.OptionMapper;
+import p1admin.adminDB.DBFacade;
+import p1admin.adminDB.GenericDBFacade;
 import p1admin.adminDB.PreguntaMapper;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import p1admin.model.Opcion;
+import p1admin.model.Pregunta;
 
 public class ConnectionTest {
 
@@ -19,17 +22,31 @@ public class ConnectionTest {
         cpds.setAcquireRetryDelay(1);
         cpds.setBreakAfterAcquireFailure(true);
 
+        GenericDBFacade<Pregunta, Opcion> facade = new DBFacade(cpds);
         PreguntaMapper pm = new PreguntaMapper(cpds);
         Object[] values = new Object[] {"¿Te gusta el pimiento?"};
         pm.insert(values);
 
-        OptionMapper om = new OptionMapper(cpds);
-        om.insert(new Object[] {1, 1, "This is a question"});
+        Pregunta p = new Pregunta();
+        p.setId(1);
+        p.setEnunciado("Te gusta el pimiento?");
 
-        om.update(new Object[] {1, "Ok now watch this!"}, new Object[] {1, 1});
-        System.out.println(om.findById(new Object[] {1, 1}));
+        Opcion op = new Opcion();
+        Opcion op1 = new Opcion();
+        op.setTexto("This is a question");
+        op1.setTexto("This is another question");
 
-        pm.delete(new Object[] {1});
+        p.addOpcion(op);
+        p.addOpcion(op1);
+
+        facade.insertAnswer(p, op);
+        facade.insertAnswer(p, op1);
+
+        op.setTexto("Booya!");
+        p.intercambiarOpciones(1,2);
+        facade.updateAnswer(p, op);
+        facade.updateAnswer(p, op1);
+
         cpds.close();
     }
 }
