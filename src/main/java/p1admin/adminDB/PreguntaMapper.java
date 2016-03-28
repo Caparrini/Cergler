41acdac47a48af928f86f2609a01d5a659c4ed69
Caparrini <Caparrini@users.noreply.github.com>
2016-03-28 18:24:33 +0200

    Modified PreguntaMapper, added method for get all the questions with their answers

--- src/main/java/p1admin/adminDB/PreguntaMapper.java
+++ src/main/java/p1admin/adminDB/PreguntaMapper.java
@@ -1,10 +1,15 @@
 package p1admin.adminDB;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.sql.DataSource;
 
+import p1admin.model.Opcion;
 import p1admin.model.Pregunta;
 
 public class PreguntaMapper extends AbstractMapper<Pregunta>{
@@ -38,7 +43,7 @@ public class PreguntaMapper extends AbstractMapper<Pregunta>{
             throws SQLException {
         //TODO
         int qId = rs.getInt(getKeyColumnName());
-        String content = rs.getString("content");
+        String content = rs.getString(getColumnNames()[0]);
         
         Pregunta p = new Pregunta();
         p.setId(qId);
@@ -72,4 +77,65 @@ public class PreguntaMapper extends AbstractMapper<Pregunta>{
 		this.update(cv, kv);
 		
 	}
+	
+    /**
+     * Select All
+     * @return Lista de Preguntas de una tabla con sus respuestas
+     */
+     public List<Pregunta> selectAllWithOptions() {
+         List<Pregunta> res = new LinkedList<Pregunta>();
+         OptionMapper op = new OptionMapper(ds);
+         String sql = "SELECT * FROM "+ this.getTableName() +" LEFT JOIN answers ON " + this.getTableName()+".id = answers.questionId";
+
+         //TODO
+         try (Connection con = ds.getConnection();
+             PreparedStatement pst = con.prepareStatement(sql)) {
+
+             ResultSet rs = pst.executeQuery();
+             int lastId = -1;
+             int currentId = -1;
+        	 Pregunta p = new Pregunta();
+        	 Pregunta paux = new Pregunta();
+        	 Opcion o = new Opcion();
+
+
+
+             while(rs.next()){ //While exist another line
+            	 
+            	 paux=this.buildObjectFromResultSet(rs);		
+            	 o = op.buildObjectFromResultSet(rs);
+            	 
+            	 currentId = paux.getId();
+            	 
+
+            	 if(lastId!=currentId){//If id is !equal to last id is a new question
+            		 if(lastId!=-1){
+                		 res.add(p); //added pregunta
+            		 }
+            		 lastId = currentId;
+            		 
+            		 p=paux;
+
+            	 }//else (qId = lId then is another option for the last question
+
+            	 
+            	 if(o.getNumeroOrden()!=0){
+        			 p.addOpcion(o);	
+        		 }
+            	
+           	 }
+             
+             res.add(p);
+             
+         } catch (SQLException e) {
+             e.printStackTrace();
+             
+         }
+		return res;
+         
+
+     }
+     
+     
+     
 }
